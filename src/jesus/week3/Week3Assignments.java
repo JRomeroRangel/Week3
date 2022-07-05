3fa57eb8068bb4c54bbb01dbc4ca325e9671618a
JRomeroRangel <aromero6412@gmail.com>
2022-07-04 23:04:03 -0600

    Commit assignment up to No #8

--- src/jesus/week3/Week3Assignments.java
+++ src/jesus/week3/Week3Assignments.java
@@ -1,47 +1,99 @@
 package jesus.week3;
 
+import java.util.Arrays;
+
+import javax.naming.event.NamespaceChangeListener;
+
 public class Week3Assignments {
 
 	public static void main(String[] args) {
 
-		//Part 1.	
+		// Part 1.
 		int ages[] = { 9, 23, 64, 2, 8, 28, 93 }; // creation of age array
 
 		int lastElementIndex = ages.length;
-		System.out.println("The last element of the array, minus the first element equals: " + (ages[lastElementIndex - 1] - ages[0])); // Part a 
-		
+		System.out.println("The last element of the array, minus the first element equals: "
+				+ (ages[lastElementIndex - 1] - ages[0])); // Part a
+
 		double sum = 0;
 		for (int numbers : ages) {
 			sum += numbers;
 		}
 		System.out.println("The avarage of the array is: " + String.format("%.2f", (sum / lastElementIndex))); // Part c
-		
-		// System.out.println(ages[ages.length -1]); return the last element of the array 
+
 //	Part 2. a
 
-		String names[] = { "Sam", "Tommy", "Tim", "Sally", "Buck", "Bob" }; 
+		String names[] = { "Sam", "Tommy", "Tim", "Sally", "Buck", "Bob" };
 		int index = 0;
-		
-		for(String letters: names) {
+
+		for (String letters : names) {
 			index += letters.length();
-			
+
 		}
-		
-		
-		System.out.println("The avarage number of letters in the array is: " + index/names.length);
-		
-		
-		//Part b, Concatenate elements of the array 
-		
-		String sum2 = ""; 
-		
-		for (String name: names) {
-			sum2 += (name +" ");
+
+		System.out.println("The avarage number of letters in the array is: " + index / names.length);
+
+		// Part b, Concatenate elements of the array
+
+		String sum2 = "";
+
+		for (String name : names) {
+			sum2 += (name + " ");
 		}
 		System.out.println(sum2);
+
+//Part 5
+		int nameLengths[] = new int[names.length];
+		int j = 0;
+		int k = 0;
+		for (String letters : names) {
+			j = letters.length();
+			nameLengths[k] = j;
+			k++;
+		}
+
+		System.out.println(Arrays.toString(nameLengths));
+
+//Part 6 
+		int sum5 = 0;
 		
+		for(int count: nameLengths) {
+			sum5 += count;
+			
+		}
+		System.out.println("Sum of all the elements in the array is: " + sum5);
 		
 
+
+//Part 7 Method Return		
+		System.out.println(myword("Hello", 3));
+		
+//Part 8 Method Return
+		System.out.println(myName("Jeus", "Romero"));
+		
+		
+		
+	
+		}
+//---------------------------------------------------------Main Method Break_____________________________________________________________	
+// Part 7
+	public static String myword(String word , int n) {
+		String answer= "";
+		for (int i = 0; i < n; i++) {
+	    		   answer += word;
+		}
+	
+		return answer;
 	}
-
+	
+//Part 8
+	public static String myName(String name1, String name2 ) {
+		
+		String answer = name1 + " " + name2;
+		
+		return answer;
+	}
+	
+	
+	
 }
