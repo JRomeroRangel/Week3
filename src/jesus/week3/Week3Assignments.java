885f80696b62b17978b1d9e25e885761cc217e3c
JRomeroRangel <aromero6412@gmail.com>
2022-07-04 21:36:32 -0600

    first

---
+++ src/jesus/week3/Week3Assignments.java
@@ -0,0 +1,43 @@
+package jesus.week3;
+
+public class Week3Assignments {
+
+	public static void main(String[] args) {
+
+		//Part 1.	
+		int ages[] = { 9, 23, 64, 2, 8, 28, 93 }; // creation of age array
+
+		int lastElementIndex = ages.length;
+		System.out.println("The last element of the array, minus the first element equals: " + (ages[lastElementIndex - 1] - ages[0])); // Part a 
+		
+		double sum = 0;
+		for (int numbers : ages) {
+			sum += numbers;
+		}
+		System.out.println("The avarage of the array is: " + String.format("%.2f", (sum / lastElementIndex))); // Part c
+		
+//	Part 2. a
+
+		String names[] = { "Sam", "Tommy", "Tim", "Sally", "Buck", "Bob" }; 
+		int index = 0;
+		
+		for(String letters: names) {
+			index += letters.length();
+			
+		}
+		
+		System.out.println("The avarage number of letters in the array is: " + (double)index/names.length);
+		
+		
+		//Part b, Concatenate elements of the array 
+		
+		String sum2 = ""; 
+		
+		for (String name: names) {
+			sum2 += (name +" ");
+		}
+		System.out.println(sum2);
+
+	}
+
+}
